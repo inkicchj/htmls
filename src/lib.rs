e676e6dbc9633ed0890633159e14013eec4857b7
inkicchj <399434428@qq.com>
2025-04-28 17:11:24 +0800

    improved the index selection feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,9 @@
 mod lexer;
 mod parser;
-mod interpreter;
-mod query;
+pub mod interpreter;
+pub mod query;
 
 use lexer::*;
 use interpreter::*;
 use parser::*;
 pub use query::Query;
-
-
-#[cfg(test)]
-mod tests {
-
-    use super::Query;
-
-    #[test]
-    fn main_test() {
-
-
-        let html = r#"
-        <div class="a">
-            <p>text 1</p>
-            <p>text 2</p>
-            <p>text 3</p>
-        </div>
-        <div class="b">
-            <p>text 4</p>
-            <p>text 5</p>
-            <p>text 6</p>
-        </div>
-        "#;
-        let q = Query::new(html);
-        let result = q.query(r#"tag p:-1::-2 > text @replace," ","""#).texts();
-        println!("{:?}", result); // ["text2", "text3", "text4"]
-
-    }
-
-}
